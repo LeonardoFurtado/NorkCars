4be3d583d873787f7cd688abe7f7a014e554e7c5
Leonardo Furtado <srleonardofurtado@gmail.com>
2024-06-24 10:09:07 -0300

    tests: add new test to owners with cars.

--- tests/test_owners.py
+++ tests/test_owners.py
@@ -53,3 +53,8 @@ def test_should_list_all_owners(client, init_database, random_owner, another_ran
     assert response.status_code == 200
     assert len(response.json) == 2
 
+
+def test_should_return_owner_by_id_when_owner_have_cars(client, init_database, random_owner, auth_token, random_car):
+    response = client.get("/owners/1", headers={"Authorization": f"Bearer {auth_token}"})
+    assert response.status_code == 200
+
